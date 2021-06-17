3b8da8617008c8d6bf9f1188c3be1d713b5135de
Rakha <rakhawibowo1998@gmail.com>
2021-06-17 20:27:10 +0700

    initial commit

--- js/emailvalidation.js
+++ js/emailvalidation.js
@@ -1,7 +1,6 @@
 const email = document.querySelector("#email");
 const tombol = document.querySelector("button");
 const label = document.querySelector("label")
-const label2 = document.querySelector(".label_dekstop")
 const form = document.querySelector("form")
 
 tombol.addEventListener('click', e => {
@@ -9,35 +8,17 @@ tombol.addEventListener('click', e => {
     console.log(email.value)
     console.log(window.innerWidth)
     
-    if(window.innerWidth>1000) {
-        label.style.display = "none"
         if(validateEmail(email.value) === false){
-            email.style.border = "1px solid red"
-            label2.classList.add("error")
-            label2.style.display = "block"
-            tombol.classList.add("error_button")
-        }
-        else {
-            email.style.border = null
-            label2.classList.remove("error")
-            label2.style.display = "none"
-        }
-    }
-    if (window.innerWidth>1000){
-        label2.style.display = "none"
-        if(validateEmail(email.value) === false){
-            email.style.border = "1px solid red"
+            email.style.border = "1px solid red";
             label.classList.add("error")
             label.style.display = "block"
             tombol.classList.add("error_button")
         }
         else {
-            email.style.border = null
+            email.style.border = null;
             label.classList.remove("error")
             label.style.display = "none"
-            tombol.classList.remove("error_button")
         }
-    }
 })
 
 function validateEmail(email) {
