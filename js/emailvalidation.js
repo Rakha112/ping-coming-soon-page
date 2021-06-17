ad97238e947ecb35743209309e0d43c8a1d710a4
Rakha <rakhawibowo1998@gmail.com>
2021-06-17 20:20:28 +0700

    initial commit

--- js/emailvalidation.js
+++ js/emailvalidation.js
@@ -10,27 +10,29 @@ tombol.addEventListener('click', e => {
     console.log(window.innerWidth)
     
     if(window.innerWidth>1000) {
+        label.style.display = "none"
         if(validateEmail(email.value) === false){
-            email.style.border = "1px solid red";
+            email.style.border = "1px solid red"
             label2.classList.add("error")
             label2.style.display = "block"
             tombol.classList.add("error_button")
         }
         else {
-            email.style.border = null;
+            email.style.border = null
             label2.classList.remove("error")
             label2.style.display = "none"
         }
     }
-    else {
+    if (window.innerWidth>1000){
+        label2.style.display = "none"
         if(validateEmail(email.value) === false){
-            email.style.border = "1px solid red";
+            email.style.border = "1px solid red"
             label.classList.add("error")
             label.style.display = "block"
             tombol.classList.add("error_button")
         }
         else {
-            email.style.border = null;
+            email.style.border = null
             label.classList.remove("error")
             label.style.display = "none"
             tombol.classList.remove("error_button")
