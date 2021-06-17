ac917150fcf275ba1293a50e17f0d286b4b942fe
Rakha <rakhawibowo1998@gmail.com>
2021-06-17 19:55:47 +0700

    initial commit

--- js/emailvalidation.js
+++ js/emailvalidation.js
@@ -1,22 +1,40 @@
 const email = document.querySelector("#email");
 const tombol = document.querySelector("button");
 const label = document.querySelector("label")
+const label2 = document.querySelector(".label_dekstop")
 const form = document.querySelector("form")
 
 tombol.addEventListener('click', e => {
     e.preventDefault();
     console.log(email.value)
-    if(validateEmail(email.value) === false){
-        email.style.border = "1px solid red";
-        label.classList.add("error")
-        label.style.display = "block"
-        tombol.classList.add("error_button")
+    console.log(window.innerWidth)
+    
+    if(window.innerWidth>1000) {
+        if(validateEmail(email.value) === false){
+            email.style.border = "1px solid red";
+            label2.classList.add("error")
+            label2.style.display = "block"
+            tombol.classList.add("error_button")
+        }
+        else {
+            email.style.border = null;
+            label2.classList.remove("error")
+            label2.style.display = "none"
+        }
     }
     else {
-        email.style.border = null;
-        label.classList.remove("error")
-        label.style.display = "none"
-        tombol.classList.remove("error_button")
+        if(validateEmail(email.value) === false){
+            email.style.border = "1px solid red";
+            label.classList.add("error")
+            label.style.display = "block"
+            tombol.classList.add("error_button")
+        }
+        else {
+            email.style.border = null;
+            label.classList.remove("error")
+            label.style.display = "none"
+            tombol.classList.remove("error_button")
+        }
     }
 })
 
